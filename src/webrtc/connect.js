dd73b58ce5f32869d20cc1e0c634fcf374ba04b9
TateB <me@tatebulic.com.au>
2021-10-25 14:35:11 +1100

    added small webrtc fixes

--- src/webrtc/connect.js
+++ src/webrtc/connect.js
@@ -105,8 +105,6 @@ export async function connect() {
         iceServers: [
           { urls: "stun:stun1.l.google.com:19302" },
           { urls: "stun:stun2.l.google.com:19302" },
-          { urls: "stun:stun3.l.google.com:19302" },
-          { urls: "stun:stun4.l.google.com:19302" },
         ],
       },
     })
